cd4367490c5a47db7ee5fdfe5fcba4ef5719d6a7
Martin Zurowietz <martin@zurowietz.de>
2023-05-10 10:25:50 +0200

    Remove JS

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,8 +17,7 @@ mix.options({processCssUrls: false});
 
 mix.setPublicPath('src/public');
 
-mix.js('src/resources/assets/js/main.js', 'assets/scripts').vue()
-    .sass('src/resources/assets/sass/main.scss', 'assets/styles')
+mix.sass('src/resources/assets/sass/main.scss', 'assets/styles')
     .publish({
         provider: 'Biigle\\Modules\\UserDisks\\UserDisksServiceProvider',
         tag: 'public',
