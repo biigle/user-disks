78bcb33e45dca46f8872b687a98ffbee7bf3068c
Martin Zurowietz <martin@zurowietz.de>
2023-05-11 10:26:25 +0200

    Fix tests

--- tests/Http/Controllers/Views/UserDiskControllerTest.php
+++ tests/Http/Controllers/Views/UserDiskControllerTest.php
@@ -9,24 +9,24 @@ class UserDiskControllerTest extends ApiTestCase
 {
     public function testIndex()
     {
-        $this->get('settings/storage-disks')->assertStatus(302);
+        $this->get('storage-disks')->assertStatus(302);
 
         $this->beGlobalGuest();
-        $this->get('settings/storage-disks')->assertStatus(403);
+        $this->get('storage-disks')->assertStatus(403);
 
         $this->beUser();
-        $this->get('settings/storage-disks')->assertStatus(200);
+        $this->get('storage-disks')->assertStatus(200);
     }
 
     public function testCreate()
     {
-        $this->get('settings/storage-disks/create')->assertStatus(302);
+        $this->get('storage-disks/create')->assertStatus(302);
 
         $this->beGlobalGuest();
-        $this->get('settings/storage-disks/create')->assertStatus(403);
+        $this->get('storage-disks/create')->assertStatus(403);
 
         $this->beUser();
-        $this->get('settings/storage-disks/create')->assertStatus(200);
+        $this->get('storage-disks/create')->assertStatus(200);
     }
 
     public function testUpdate()
@@ -40,12 +40,12 @@ class UserDiskControllerTest extends ApiTestCase
                 'bucket' => 'example',
              ],
         ]);
-        $this->get("settings/storage-disks/{$disk->id}")->assertStatus(302);
+        $this->get("storage-disks/{$disk->id}")->assertStatus(302);
 
         $this->beUser();
-        $this->get("settings/storage-disks/{$disk->id}")->assertStatus(403);
+        $this->get("storage-disks/{$disk->id}")->assertStatus(403);
 
         $this->be($disk->user);
-        $this->get("settings/storage-disks/{$disk->id}")->assertStatus(200);
+        $this->get("storage-disks/{$disk->id}")->assertStatus(200);
     }
 }
